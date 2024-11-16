957aed4f059190c85d88296ff483d24636b7d42a
bowook <mbosx316@naver.com>
2024-11-16 19:06:08 +0900

    feat(ErrorMessage) : 에러메시지 enum 추가

--- src/main/java/subway/exception/ErrorMessage.java
+++ src/main/java/subway/exception/ErrorMessage.java
@@ -1,2 +1,16 @@
-package subway.exception;public class ErrorMessage {
+package subway.exception;
+
+public enum ErrorMessage {
+    UNABLE_TO_CHOOSE_FUNCTION("선택할 수 없는 기능입니다."),
+    ALREADY_REGISTER_STATION_NAME("이미 등록된 역 이름입니다.");
+
+    private final String errorMessage;
+
+    ErrorMessage(final String errorMessage) {
+        this.errorMessage = errorMessage;
+    }
+
+    public String getErrorMessage() {
+        return "[ERROR] " + errorMessage;
+    }
 }
