fec74a78ad6afb99b0c3e4486342d0a36ebd9a8d
bowook <mbosx316@naver.com>
2024-11-16 21:05:05 +0900

    feat(StationSelectionDTO) : DTO 기능 추가

--- src/main/java/subway/dto/StationSelectionDTO.java
+++ src/main/java/subway/dto/StationSelectionDTO.java
@@ -1,2 +1,25 @@
-package subway.dto;public class StationSelectionDTO {
+package subway.dto;
+
+import java.util.List;
+import subway.exception.ErrorMessage;
+import subway.exception.SubwayException;
+
+public class StationSelectionDTO {
+    private final List<String> functionInventory = List.of("1", "2", "3", "B");
+    private final String selection;
+
+    public StationSelectionDTO(String selection) {
+        validate(selection);
+        this.selection = selection;
+    }
+
+    public String getSelection() {
+        return selection;
+    }
+
+    private void validate(String selection) {
+        if (!functionInventory.contains(selection)) {
+            throw SubwayException.from(ErrorMessage.UNABLE_TO_CHOOSE_FUNCTION);
+        }
+    }
 }
