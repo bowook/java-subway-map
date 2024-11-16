cf270e54986882311fe25461b0fe22914ce9eb2c
bowook <mbosx316@naver.com>
2024-11-16 19:05:37 +0900

    feat(MainSelectionDTO) : Main 기능 선택 DTO 추가

--- src/main/java/subway/dto/MainSelectionDTO.java
+++ src/main/java/subway/dto/MainSelectionDTO.java
@@ -1,16 +1,26 @@
 package subway.dto;
 
 import java.util.List;
+import subway.exception.ErrorMessage;
+import subway.exception.SubwayException;
 
-public class FunctionSelectionDTO {
+public class MainSelectionDTO {
     private final List<String> functionInventory = List.of("1", "2", "3", "4", "Q");
-    private final String number;
+    private final String selection;
 
-    public FunctionSelectionDTO(String number) {
-        validate(number);
-        this.number = number;
+    public MainSelectionDTO(String selection) {
+        validate(selection);
+        this.selection = selection;
     }
 
-    private void validate(String number) {
+    public String getSelection() {
+        return selection;
     }
+
+    private void validate(String selection) {
+        if (!functionInventory.contains(selection)) {
+            throw SubwayException.from(ErrorMessage.UNABLE_TO_CHOOSE_FUNCTION);
+        }
+    }
+
 }
