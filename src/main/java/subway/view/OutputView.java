708d83fc5c2ad7ba5108f774a6a8af84c9772a53
bowook <mbosx316@naver.com>
2024-11-16 19:04:35 +0900

    feat(OutputView) : OutputView 기능 추가

--- src/main/java/subway/view/OutputView.java
+++ src/main/java/subway/view/OutputView.java
@@ -1,2 +1,28 @@
-package subway.view;public class OutputView {
+package subway.view;
+
+public class OutputView {
+    private final static String MAIN_SCREEN = "## 메인 화면";
+    private final static String STATION_MANAGEMENT = "1. 역 관리";
+    private final static String ROUTE_MANAGEMENT = "2. 노선 관리";
+    private final static String PANEL_MANAGEMENT = "3. 구간 관리";
+    private final static String WRITE_SUBWAY_ROUTE = "4. 지하철 노선도 출력";
+    private final static String EXIT = "Q. 종료";
+
+    public void writeMainScreen() {
+        System.out.println(MAIN_SCREEN);
+        System.out.println(STATION_MANAGEMENT);
+        System.out.println(ROUTE_MANAGEMENT);
+        System.out.println(PANEL_MANAGEMENT);
+        System.out.println(WRITE_SUBWAY_ROUTE);
+        System.out.println(EXIT);
+        System.out.print(writeNewLine());
+    }
+
+    public void writeErrorMessage(String errorMessage) {
+        System.out.println(errorMessage);
+    }
+
+    private String writeNewLine() {
+        return System.lineSeparator();
+    }
 }
