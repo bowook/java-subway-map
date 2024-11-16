018a4e55b8039d8c84769b01d5e285767f5a9da0
bowook <mbosx316@naver.com>
2024-11-16 21:04:05 +0900

    feat(StationService) : StationService 기능 추가

--- src/main/java/subway/service/StationService.java
+++ src/main/java/subway/service/StationService.java
@@ -1,2 +1,15 @@
-package subway.service;public class StationService {
+package subway.service;
+
+import subway.domain.Station;
+import subway.exception.ErrorMessage;
+import subway.exception.SubwayException;
+import subway.repository.StationRepository;
+
+public class StationService {
+    public void registerStation(String stationName) {
+        if (StationRepository.existsByName(stationName)) {
+            throw SubwayException.from(ErrorMessage.ALREADY_REGISTER_STATION_NAME);
+        }
+        StationRepository.addStation(new Station(stationName));
+    }
 }
