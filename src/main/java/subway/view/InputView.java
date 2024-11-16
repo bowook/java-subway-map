dcc1bc9dbea39ee16c67df00e6ee425bd7b557af
bowook <mbosx316@naver.com>
2024-11-16 21:02:51 +0900

    feat(InputView) : 지하철 역 이름 입력 받기 기능 추가

--- src/main/java/subway/view/InputView.java
+++ src/main/java/subway/view/InputView.java
@@ -4,14 +4,28 @@ import java.util.Scanner;
 
 public class InputView {
     private final static String CHOOSE_HOPE_FUNCTION = "## 원하는 기능을 선택하세요.";
+    private final static String REGISTER_STATION = "## 등록할 역 이름을 입력하세요.";
 
     public String readHopeFunction() {
         System.out.println(CHOOSE_HOPE_FUNCTION);
         return scannerInput();
     }
 
+    public String readStationName() {
+        System.out.print(System.lineSeparator());
+        System.out.println(REGISTER_STATION);
+        return scannerInput();
+    }
+
     private String scannerInput() {
-        final Scanner scanner = new Scanner(System.in);
-        return scanner.nextLine();
+        return getScanner().nextLine();
+    }
+
+    private Scanner getScanner() {
+        return new Scanner(System.in);
+    }
+
+    private void scannerClose() {
+        getScanner().close();
     }
 }
