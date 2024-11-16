6a67bc4bf92a6d2f0e9149f4c83eafe93b819cbf
bowook <mbosx316@naver.com>
2024-11-16 21:05:27 +0900

    feat(Station) : 역 이름 검증 기능 추가

--- src/main/java/subway/domain/Station.java
+++ src/main/java/subway/domain/Station.java
@@ -1,9 +1,13 @@
 package subway.domain;
 
+import subway.exception.ErrorMessage;
+import subway.exception.SubwayException;
+
 public class Station {
     private String name;
 
     public Station(String name) {
+        validate(name);
         this.name = name;
     }
 
@@ -11,5 +15,24 @@ public class Station {
         return name;
     }
 
-    // 추가 기능 구현
+    private void validate(String name) {
+        validateEmpty(name);
+        validateLength(name);
+    }
+
+    private void validateLength(String name) {
+        if (name.length() < 2) {
+            throw SubwayException.from(ErrorMessage.SUBWAY_STATION_NAME_LENGTH);
+        }
+    }
+
+    private void validateEmpty(String name) {
+        if (name == null || name.isEmpty() || name.isBlank()) {
+            throw SubwayException.from(ErrorMessage.SUBWAY_STATION_EMPTY_NAME);
+        }
+        if (name.contains(" ")) {
+            throw SubwayException.from(ErrorMessage.SUBWAY_STATION_NAME_CONTAINS_EMPTY_NAME);
+        }
+    }
+
 }
