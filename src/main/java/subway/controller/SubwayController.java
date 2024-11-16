8aae31cc90ff0f2e264bca0bb8d452d2a662fb57
bowook <mbosx316@naver.com>
2024-11-16 19:05:01 +0900

    feat(SubwayController) : 컨트롤러 기능 추가

--- src/main/java/subway/controller/SubwayController.java
+++ src/main/java/subway/controller/SubwayController.java
@@ -1,2 +1,42 @@
-package subway.controller;public class SubwayController {
+package subway.controller;
+
+import subway.dto.MainSelectionDTO;
+import subway.exception.SubwayException;
+import subway.service.SelectionService;
+import subway.view.InputView;
+import subway.view.OutputView;
+
+public class SubwayController {
+
+    private final InputView inputView;
+    private final OutputView outputView;
+    private final SelectionService selectionService;
+
+    public SubwayController(final InputView inputView, final OutputView outputView, final SelectionService service) {
+        this.inputView = inputView;
+        this.outputView = outputView;
+        this.selectionService = service;
+    }
+
+    public void start() {
+        while (true) {
+            outputView.writeMainScreen();
+            MainSelectionDTO selectionDTO = getValidatedSelection();
+            if (selectionDTO.getSelection().equals("Q")) {
+                break;
+            }
+
+        }
+    }
+
+    private MainSelectionDTO getValidatedSelection() {
+        while (true) {
+            try {
+                return new MainSelectionDTO(inputView.readHopeFunction());
+            } catch (SubwayException subwayException) {
+                outputView.writeErrorMessage(subwayException.getMessage());
+            }
+        }
+    }
+
 }
