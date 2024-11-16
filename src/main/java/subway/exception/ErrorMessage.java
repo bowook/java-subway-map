68d1c6a9c34769f7c06f87817f9f5c05efaa377c
bowook <mbosx316@naver.com>
2024-11-16 21:04:24 +0900

    feat(ErrorMessage) : 에러 메시지 추가

--- src/main/java/subway/exception/ErrorMessage.java
+++ src/main/java/subway/exception/ErrorMessage.java
@@ -2,7 +2,11 @@ package subway.exception;
 
 public enum ErrorMessage {
     UNABLE_TO_CHOOSE_FUNCTION("선택할 수 없는 기능입니다."),
-    ALREADY_REGISTER_STATION_NAME("이미 등록된 역 이름입니다.");
+    ALREADY_REGISTER_STATION_NAME("이미 등록된 역 이름입니다."),
+    SUBWAY_STATION_NAME_LENGTH("역 이름은 2글자 이상이어야 합니다."),
+    SUBWAY_STATION_NAME_NUMBER("역 이름은 숫자가 들어갈 수 없습니다."),
+    SUBWAY_STATION_EMPTY_NAME("역 이름은 비어있을 수 없습니다."),
+    SUBWAY_STATION_NAME_CONTAINS_EMPTY_NAME("역 이름은 비어있는 문자열이 들어갈 수 없습니다.");
 
     private final String errorMessage;
 
