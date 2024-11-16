04f939286189558d1529e03d6e83cd89e5861fc5
bowook <mbosx316@naver.com>
2024-11-16 19:04:14 +0900

    feat(InputView) : InputView 기능 추가

--- src/main/java/subway/view/InputView.java
+++ src/main/java/subway/view/InputView.java
@@ -1,2 +1,17 @@
-package subway.view;public class InputView {
+package subway.view;
+
+import java.util.Scanner;
+
+public class InputView {
+    private final static String CHOOSE_HOPE_FUNCTION = "## 원하는 기능을 선택하세요.";
+
+    public String readHopeFunction() {
+        System.out.println(CHOOSE_HOPE_FUNCTION);
+        return scannerInput();
+    }
+
+    private String scannerInput() {
+        final Scanner scanner = new Scanner(System.in);
+        return scanner.nextLine();
+    }
 }
